d2cce416ad19c0e2faec9f564e32344338e72ffb
wanghao <491993108@qq.com>
2018-10-11 15:22:45 +0800

    拉取组件运行laravel-echo-server服务

---
+++ resources/views/echo.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Styles -->
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+
+</div>
+</body>
+</html>
